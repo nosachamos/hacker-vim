b4b9448515bf4de7073a5159f2361831b9f2ac53
Eduardo Born <nosachamos@gmail.com>
2025-05-31 15:48:54 -0500

    Updating installation to use NvChad

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hacker-vim
+
+Run the installation process:
+
+```
+curl -sSL https://raw.githubusercontent.com/nosachamos/hacker-vim/master/install.sh | bash
+```
+
--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 # Check if Neovim is already installed
-if ! command -v nvim >/dev/null 2>&1; then
-  echo "Neovim not found. Installing..."
-
-  # Add official Neovim PPA
-  sudo add-apt-repository -y ppa:neovim-ppa/stable
-  sudo apt update
-
-  # Install Neovim
-  sudo apt install -y neovim
-else
-  echo "Neovim is already installed. Skipping installation."
+if command -v nvim >/dev/null 2>&1; then
+  echo "Neovim is already installed. Will remove existing installation..."
+  sudo apt remove --purge neovim
 
   # Backup existing Neovim configuration
   mv ~/.config/nvim{,.bak} 2>/dev/null
@@ -20,5 +12,14 @@ else
   mv ~/.cache/nvim{,.bak} 2>/dev/null
 fi
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
+echo "Installing Neovim..."
+
+# Add official Neovim PPA
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt update
+
+# Install Neovim and dependencies
+sudo apt install neovim
+
+git clone https://github.com/NvChad/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
