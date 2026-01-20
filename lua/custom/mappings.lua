a8a81a9d6f46eaf494ecfc500ec42b5c4362e493
Eduardo Born <nosachamos@gmail.com>
2026-01-20 16:33:07 -0600

    Adding several convinience key bindings for working with the system clipboard.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,5 +1,27 @@
 local map = vim.keymap.set
 
+local function get_visual_selection()
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+  if start_pos[2] == 0 or end_pos[2] == 0 then
+    return ""
+  end
+
+  if start_pos[2] > end_pos[2] or (start_pos[2] == end_pos[2] and start_pos[3] > end_pos[3]) then
+    start_pos, end_pos = end_pos, start_pos
+  end
+
+  local lines = vim.api.nvim_buf_get_text(
+    0,
+    start_pos[2] - 1,
+    start_pos[3] - 1,
+    end_pos[2] - 1,
+    end_pos[3],
+    {}
+  )
+  return table.concat(lines, "\n"):gsub("\n", " ")
+end
+
 local function dap_call(fn_name)
   return function()
     local ok, dap = pcall(require, "dap")
@@ -16,7 +38,16 @@ map("n", "<F11>", dap_call("step_into"), { desc = "DAP step into" })
 map("n", "<F12>", dap_call("step_out"), { desc = "DAP step out" })
 
 map("v", "<leader>fg", function()
-  require("telescope.builtin").grep_string({
-    search = vim.fn.getreg("v"),
-  })
+  local text = get_visual_selection()
+  vim.cmd("normal! <Esc>")
+  if text ~= "" then
+    require("telescope.builtin").live_grep({
+      default_text = text,
+    })
+  end
 end, { desc = "Grep visual selection", silent = true })
+
+map("v", "<C-c>", '"+y', { desc = "Copy to system clipboard", silent = true })
+map("n", "<C-v>", '"+p', { desc = "Paste from system clipboard", silent = true })
+map("i", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard", silent = true })
+map("v", "<C-v>", '"_d"+P', { desc = "Paste over selection from system clipboard", silent = true })
